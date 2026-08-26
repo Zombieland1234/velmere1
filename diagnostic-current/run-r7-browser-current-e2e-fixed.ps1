39d2973b84e2d0a5b7ca55e02b0b795dec3d26f2
Zombieland1234 <velmere141@gmail.com>
2026-08-26 21:56:25 +0200

    R7 current: align E2E canonical localhost Host header

--- diagnostic-current/run-r7-browser-current-e2e-fixed.ps1
+++ diagnostic-current/run-r7-browser-current-e2e-fixed.ps1
@@ -6,9 +6,12 @@ $OldCheck = 'git apply --check --no-index $PatchPath'
 $NewCheck = 'git -c core.autocrlf=false apply --check --no-index $PatchPath'
 $OldApply = 'git apply --no-index $PatchPath'
 $NewApply = 'git -c core.autocrlf=false apply --no-index $PatchPath'
+$OldBase = "$env:R7_E2E_BASE_URL = 'http://127.0.0.1:3100'"
+$NewBase = "$env:R7_E2E_BASE_URL = 'http://localhost:3100'"
 if (([regex]::Matches($Text, [regex]::Escape($OldCheck))).Count -ne 1) { throw 'e2e_patch_check_anchor_mismatch' }
 if (([regex]::Matches($Text, [regex]::Escape($OldApply))).Count -ne 1) { throw 'e2e_patch_apply_anchor_mismatch' }
-$Text = $Text.Replace($OldCheck, $NewCheck).Replace($OldApply, $NewApply)
+if (([regex]::Matches($Text, [regex]::Escape($OldBase))).Count -ne 1) { throw 'e2e_base_url_anchor_mismatch' }
+$Text = $Text.Replace($OldCheck, $NewCheck).Replace($OldApply, $NewApply).Replace($OldBase, $NewBase)
 Set-Content -LiteralPath $Fixed -Value $Text -Encoding utf8
 & $Fixed
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
